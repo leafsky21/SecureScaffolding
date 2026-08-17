a40668bd8ed0330bc7631f6d923988117811e76b
leafsky21 <leafsky@live.com.au>
2026-08-17 12:54:30 +1000

    Fix static Cloudflare Pages build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
-
 export default defineConfig({
   site: 'https://www.securescaffolding.com.au',
-  adapter: cloudflare({ mode: 'static' }),
   output: 'static',
   trailingSlash: 'never'
 });
